1eeb6abed86e4d064f937cb7af8de4ada5bd90ae
Makar8000 <Makar8000@users.noreply.github.com>
2023-03-25 05:44:11 -0500

    add remove command function

--- utils/deploy-commands.js
+++ utils/deploy-commands.js
@@ -9,43 +9,39 @@ const commands = [];
 const commandsPath = path.join(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(path.join(commandsPath, file));
-	commands.push(command.data.toJSON());
+if (!args.remove) {
+	for (const file of commandFiles) {
+		const command = require(path.join(commandsPath, file));
+		commands.push(command.data.toJSON());
+	}
 }
 
 // const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-(async () => {
-	try {
-		if (!args.local) {
-			logger.info(`Started refreshing ${commands.length} application (/) commands globally.`);
-
+const deploy = async (deployAll) => {
+	const action = args.remove ? 'delet' : 'deploy';
+	const cmdLen = args.remove ? 'all' : commands.length;
+	if (deployAll) {
+		logger.info(`Started ${action}ing ${cmdLen} application (/) command(s) globally.`);
+		/*
+    await rest.put(
+      Routes.applicationCommands(process.env.DISCORD_APP_ID),
+      { body: commands },
+    );
+    */
+		logger.info(`Successfully ${action}ed ${cmdLen} application (/) command(s) globally.`);
+	}
+	else {
+		for (const [guildName, guildId] of Object.entries(config.guilds)) {
+			logger.info(`Started ${action}ing ${cmdLen} application (/) command(s) for ${guildName}.`);
 			/*
-      const data = await rest.put(
-        Routes.applicationCommands(process.env.DISCORD_APP_ID),
+      await rest.put(
+        Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, guildId),
         { body: commands },
       );
       */
-
-			logger.info(`Successfully reloaded ${commands.length} application (/) commands globally.`);
+			logger.info(`Successfully ${action}ed ${cmdLen} application (/) command(s) for ${guildName}.`);
 		}
-		else {
-			for (const [guildName, guildId] of Object.entries(config.guilds)) {
-				logger.info(`Started refreshing ${commands.length} application (/) commands into ${guildName}.`);
-
-				/*
-        const data = await rest.put(
-          Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, guildId),
-          { body: commands },
-        );
-        */
-
-				logger.info(`Successfully reloaded ${commands.length} application (/) commands into ${guildName}.`);
-			}
-		}
-
 	}
-	catch (error) {
-		logger.error(error);
-	}
-})();
+};
+
+deploy(!args.local);
