0f2bb6f13346ff832af359e2fee13253197566f6
Makar8000 <Makar8000@users.noreply.github.com>
2023-03-25 07:56:11 -0500

    enable deploy-command

--- utils/deploy-commands.js
+++ utils/deploy-commands.js
@@ -1,5 +1,4 @@
 const { REST, Routes, Collection } = require('discord.js');
-const fs = require('fs');
 const path = require('path');
 const config = require(path.join(__dirname, '../config.json'));
 const logger = require(path.join(__dirname, 'logger.js'));
@@ -11,30 +10,26 @@ if (!args.remove) {
 	parseCommands(path.join(__dirname, '../commands'), collection);
 }
 
-// const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 const deploy = async (deployAll) => {
 	const commands = Array.from(collection.values()).map(c => c.data.toJSON());
 	const action = args.remove ? 'delet' : 'deploy';
 	const cmdLen = args.remove ? 'all' : commands.length;
 	if (deployAll) {
 		logger.info(`Started ${action}ing ${cmdLen} application (/) command(s) globally.`);
-		/*
-    await rest.put(
-      Routes.applicationCommands(process.env.DISCORD_APP_ID),
-      { body: commands },
-    );
-    */
+		await rest.put(
+			Routes.applicationCommands(process.env.DISCORD_APP_ID),
+			{ body: commands },
+		);
 		logger.info(`Successfully ${action}ed ${cmdLen} application (/) command(s) globally.`);
 	}
 	else {
 		for (const [guildName, guildId] of Object.entries(config.guilds)) {
 			logger.info(`Started ${action}ing ${cmdLen} application (/) command(s) for ${guildName}.`);
-			/*
-      await rest.put(
-        Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, guildId),
-        { body: commands },
-      );
-      */
+			await rest.put(
+				Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, guildId),
+				{ body: commands },
+			);
 			logger.info(`Successfully ${action}ed ${cmdLen} application (/) command(s) for ${guildName}.`);
 		}
 	}
