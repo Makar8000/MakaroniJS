74bfb60f44dd0e5898d16207cef69c897c222768
Makar8000 <Makar8000@users.noreply.github.com>
2023-03-28 00:21:32 -0500

    use pm2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const { Client, Collection, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
 const logger = require(path.join(__dirname, 'utils/logger.js'));
 const parseCommands = require(path.join(__dirname, 'utils/parse-commands.js'));
 
