86a095fe3db9d7b391ff20b7bf7c8d6229e25603
Makar8000 <Makar8000@users.noreply.github.com>
2023-04-10 23:56:17 -0500

    add some utils

---
+++ utils/common.js
@@ -0,0 +1,32 @@
+/**
+ * Parses an string that may contain HTML tags into
+ * something more discord-friendly.
+ * @param {String} str
+ *  The string to manipulate
+ * @param {Number} maxLen
+ *  The max length of the string.
+ * @returns
+ *  A discord-friendly string.
+ */
+const getDiscordStr = (str, maxLen) => {
+  let newStr = str.trim();
+  // Italics
+  newStr = newStr.replaceAll(/<\/?i>/g, '_');
+  // New Lines
+  newStr = newStr.replaceAll(/<br\s?\/?>/g, '');
+  // Bold
+  newStr = newStr.replaceAll(/<\/?(?:b|strong)>/g, '**');
+  // Other tags
+  newStr = newStr.replaceAll(/<\/?[^<]+>/g, '').trim();
+
+  // Max Length
+  if (maxLen) {
+    newStr = newStr.substring(str, maxLen);
+  }
+
+  return newStr.trim();
+};
+
+module.exports = {
+  getDiscordStr,
+};
